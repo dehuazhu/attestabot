80409c6557d186908f4a2a601185d332c744fe3d
druini <daniele.ruini@cern.ch>
2022-09-06 09:33:54 +0200

    add working urlFinder

--- urlFinder.py
+++ urlFinder.py
@@ -13,6 +13,8 @@ def make_urls(firmName):
     possibleUrls = []
     firmName = firmName.lower()
     nameList = firmName.split(' ')
+    if nameList[-1] in ('liquidazione','liquidation'):
+        nameList = nameList[:-2] # remove 'in liquidazione', 'in liquidation', 'en liquidation'
     if nameList[-1] in ('sagl','sa','ag','gmbh','sàrl'):
         nameList = nameList[:-1]
     for join in urlJoins:
@@ -27,7 +29,7 @@ def url_exists(urlsToCheck,idx):
     for url in urlsToCheck:
         try:
             #print(f'trying {url}')
-            response = rq.get(url, allow_redirects=False, timeout=2)
+            response = rq.get(url, allow_redirects=False, timeout=3)
             if response.status_code==200:
                 with open('tmp_urls.txt', 'a+') as f:
                     f.write(f'{idx},{url}\n')
@@ -41,27 +43,26 @@ def threadFunc(rows):
 
 
 def main():
-    max_threads = 64
+    max_threads = 128
     files = glob( os.path.join('firms','firms*pkl') )
-    for path in files:
-        print(f'starting with {path}')
-        df = pd.read_pickle(path)
-        df['url'] = pd.Series([[] for _ in range(len(df.index))])
-        batches = linspace(0,len(df),max_threads, dtype=int)
-        threads = []
-        for start,stop in zip(batches[:-1],batches[1:]):
-            t = threading.Thread(target=threadFunc, args=(df.iloc[start:stop],))
-            threads.append(t)
-        for t in threads:
-            t.start()
-        for t in threads:
-            t.join()
-        with open('tmp_urls.txt') as f:
-            for l in f.readlines():
-                idx,url = l.strip().split(',')
-                df.iloc[int(idx)]['url'].append(url)
-        os.remove('tmp_urls.txt')
-        df.to_pickle(path)
+    df = pd.concat([pd.read_pickle( file ) for file in glob( os.path.join('firms','firms*pkl') )], ignore_index=True) # merge everything into a single dataframe
+    df['url'] = pd.Series([[] for _ in range(len(df.index))])
+    batches = linspace(0,len(df),max_threads, dtype=int)
+    threads = []
+    for start,stop in zip(batches[:-1],batches[1:]):
+        t = threading.Thread(target=threadFunc, args=(df.iloc[start:stop],))
+        threads.append(t)
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
+    with open('tmp_urls.txt') as f:
+        for l in f.readlines():
+            idx,url = l.strip().split(',')
+            df.iloc[int(idx)]['url'].append(url)
+    os.remove('tmp_urls.txt')
+    df.to_pickle( os.path.join('firms','all.pkl') )
+    df.to_excel( os.path.join('firms','all.xlsx') )
 
 
 if __name__=='__main__':
