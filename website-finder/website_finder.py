56523152dff3d12e3c23516496cfbaec59f2e69c
druini <daniele.ruini@cern.ch>
2022-10-28 17:50:55 +0200

    website_finder.py: try parallelization with pandarallel

--- website-finder/website_finder.py
+++ website-finder/website_finder.py
@@ -4,6 +4,7 @@ import requests as rq
 import os
 import threading
 from numpy import linspace
+#from pandarallel import pandarallel
 from UrlMaker import UrlMaker
 from pdb import set_trace
 
@@ -24,11 +25,28 @@ def threadFunc(rows):
         possible_urls = url_maker.make_urls(firm['name'])
         url_exists(possible_urls, idx)
 
+def check_urls(urls_to_check):
+    valid_urls = []
+    for url in urls_to_check:
+        try:
+            #print(f'trying {url}')
+            response = rq.get(url, allow_redirects=False, timeout=3)
+            if response.status_code==200:
+                valid_urls.append(url)
+        except:
+            pass
+    return valid_urls
 
 def main():
+    #pandarallel.initialize(progress_bar=True, nb_workers=200)
     max_threads = 128
     files = glob( os.path.join('..', 'firms','firms*pkl') )
     df = pd.concat([pd.read_pickle(file) for file in files], ignore_index=True) # merge everything into a single dataframe
+    #url_maker = UrlMaker()
+    #df['url'] = df.parallel_apply(lambda row:
+    #        check_urls(url_maker.make_urls(row['name'])),
+    #        axis=1
+    #        )
     df['url'] = pd.Series([[] for _ in range(len(df.index))])
     batches = linspace(0,len(df),max_threads, dtype=int)
     threads = []
@@ -44,8 +62,8 @@ def main():
             idx,url = l.strip().split(',')
             df.iloc[int(idx)]['url'].append(url)
     os.remove('tmp_urls.txt')
-    df.to_pickle( os.path.join('firms','all.pkl') )
-    df.to_excel( os.path.join('firms','all.xlsx') )
+    df.to_pickle( os.path.join('..','firms','all.pkl') )
+    df.to_excel( os.path.join('..','firms','all.xlsx') )
 
 
 if __name__=='__main__':
