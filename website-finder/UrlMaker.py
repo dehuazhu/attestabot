0a1078cb287697efadafe47f2766312e8aeddbc5
druini <daniele.ruini@cern.ch>
2022-10-28 12:03:10 +0200

    UrlMaker.py: extend logic to try partial firm names

--- website-finder/UrlMaker.py
+++ website-finder/UrlMaker.py
@@ -1,3 +1,5 @@
+import itertools
+
 class UrlMaker:
 
     def make_urls(self, firm_name):
@@ -11,10 +13,14 @@ class UrlMaker:
             name_as_list = name_as_list[:-2] # remove 'in liquidazione', 'in liquidation', 'en liquidation'
         if name_as_list[-1] in ('sagl','sa','ag','gmbh','sàrl'):
             name_as_list = name_as_list[:-1]
-        for join in url_joins:
-            baseUrl = join.join(name_as_list)
-            for tld in TLDs:
-                url = f'http://{baseUrl}.{tld}'
-                possible_urls.append(url)
-            if len(name_as_list)==1: break
+        partial_names = (name_as_list[:number_of_words] for number_of_words in range(1, min(5,len(name_as_list)+1))) #partial firm names with up to 4 words
+        if len(name_as_list)>4: #try also the full name if it has more then 4 words
+            partial_names = itertools.chain(partial_names, name_as_list)
+        for partial_name_list in partial_names:
+            for join in url_joins:
+                baseUrl = join.join(partial_name_list)
+                for tld in TLDs:
+                    url = f'http://{baseUrl}.{tld}'
+                    possible_urls.append(url)
+                if len(partial_name_list)==1: break
         return possible_urls
