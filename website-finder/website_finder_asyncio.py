0efcac89ebc0f8cd62ef1f263e410443eb9f3344
druini <daniele.ruini@cern.ch>
2022-11-04 18:00:11 +0100

    website_finder_asyncio.py now works

--- website-finder/website_finder_asyncio.py
+++ website-finder/website_finder_asyncio.py
@@ -6,15 +6,16 @@ from UrlMaker import UrlMaker
 from pdb import set_trace
 
 async def url_exists(urls_to_check,idx, session):
+    valid_urls = []
     for url in urls_to_check:
         try:
-            #print(f'trying {url}')
-            async with session.get(url, allow_redirects=False, timeout=3) as response:
-                if response.status_code==200:
-                    with open('tmp_urls.txt', 'a+') as f:
-                        f.write(f'{idx},{url}\n')
+            async with session.get(url, timeout=aiohttp.ClientTimeout(total=None, sock_connect=5, sock_read=5)) as response:
+                if response.status==200:
+                    valid_urls += [(idx,url)]
         except:
             pass
+    if len(valid_urls)>0:
+        return valid_urls
 
 async def check_all_urls(df):
     url_maker = UrlMaker()
@@ -24,18 +25,17 @@ async def check_all_urls(df):
             possible_urls = url_maker.make_urls(firm['name'])
             task = asyncio.ensure_future(url_exists(possible_urls, idx, session))
             tasks.append(task)
-        await asyncio.gather(*tasks, return_exceptions=True)
+        return await asyncio.gather(*tasks, return_exceptions=True)
 
 def main():
-    files = glob( os.path.join('..', 'firms','firms_AI*pkl') )
+    files = glob( os.path.join('..', 'firms','firms*pkl') )
     df = pd.concat([pd.read_pickle(file) for file in files], ignore_index=True) # merge everything into a single dataframe
     df['url'] = pd.Series([[] for _ in range(len(df.index))])
-    asyncio.run(check_all_urls(df))
-    with open('tmp_urls.txt') as f:
-        for l in f.readlines():
-            idx,url = l.strip().split(',')
-            df.iloc[int(idx)]['url'].append(url)
-    os.remove('tmp_urls.txt')
+    valid_urls = asyncio.run(check_all_urls(df))
+    for result in valid_urls:
+        if result is not None:
+            for idx,url in result:
+                df.iloc[int(idx)]['url'].append(url)
     df.to_pickle( os.path.join('..','firms','all.pkl') )
     df.to_excel( os.path.join('..','firms','all.xlsx') )
 
