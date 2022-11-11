bf0e4917267c13959716879e9009ba31e9fb1b3e
druini <daniele.ruini@cern.ch>
2022-11-11 18:53:03 +0100

    continue IsoFinder development, maybe finished (still has example urls)

--- attestabot/spiders/IsoFinder.py
+++ attestabot/spiders/IsoFinder.py
@@ -1,112 +1,151 @@
-from scrapy import Request, Item
+from scrapy import Request
 from scrapy.spiders import CrawlSpider, Rule
-from scrapy.linkextractors import LinkExtractor, IGNORED_EXTENSIONS
-import os
+from scrapy.linkextractors import LinkExtractor
+import os, logging
 import pandas as pd
-from glob import glob
 from urllib.parse import urlparse
 from pdb import set_trace
 
-MAX_HITS_PER_HOST = 10
+MAX_HITS_PER_HOST_PAGE_WITH_KW  = 10
+MAX_HITS_PER_HOST_PAGE_WITH_IMG = 5
+MAX_HITS_PER_HOST_TOTAL         = 20
+MAX_REQUESTS_PER_HOST           = 700
 
-KEYWORDS_URL = (
-        'ISO',
-        'Zertifikat',
-        )
-
-KEYWORDS_CERTIFICATE = (
-        'iso',
+KEYWORDS_CERTIFICATE_NO_ISO = (
         'zertifikat',
+        'zertifizierung',
+        'certifica',
         )
+
+KEYWORDS_CERTIFICATE = KEYWORDS_CERTIFICATE_NO_ISO + ('iso',)
 
 ALLOWED_EXTENSIONS = (
         'pdf',
         'png',
+        'jpg',
+        'jpeg',
         )
-
-def process_links(links):
-    set_trace()
-    yield link
 
 
 class IsoFinder(CrawlSpider):
-    name = 'isofinder'
+    name = 'iso-finder'
 
     custom_settings = {
+            #'ROBOTSTXT_OBEY'         : False,
             'LOG_LEVEL'              : 'INFO',
-            'DEPTH_LIMIT'            : 2,
-            #'DEPTH_PRIORITY'         : 1,
-            #'SCHEDULER_DISK_QUEUE'   : 'scrapy.squeues.PickleFifoDiskQueue',
-            #'SCHEDULER_MEMORY_QUEUE' : 'scrapy.squeues.FifoMemoryQueue',
+            'DEPTH_LIMIT'            : 4,
+            'DEPTH_PRIORITY'         : 1,
+            'SCHEDULER_DISK_QUEUE'   : 'scrapy.squeues.PickleFifoDiskQueue',
+            'SCHEDULER_MEMORY_QUEUE' : 'scrapy.squeues.FifoMemoryQueue',
+            'COOKIES_ENABLED'        : False,
+            'RETRY_ENABLED'          : False,
             }
 
-    ignored_extensions = IGNORED_EXTENSIONS
-    for ext in ALLOWED_EXTENSIONS:
-        ignored_extensions.remove(ext)
     rules = [
-            Rule(LinkExtractor(deny_extensions=ignored_extensions), follow=True, callback='parse_item', process_request='process_request'),
-            #Rule(LinkExtractor(deny_extensions=ignored_extensions), follow=True, callback='parse_item', process_links=process_links, process_request=process_request),
+            Rule(LinkExtractor(), follow=True, callback='parse_item', process_request='process_request'),
             ]
+
+
+    #placeholder, may be useful in the future
+    #def process_links(self, links):
+    #    for link in links:
+    #        yield link
 
 
     def process_request(self, request, response):
         hostname = urlparse(response.url).hostname
-        if self.host_counter[hostname]>MAX_HITS_PER_HOST:
+        if (hostname != urlparse(request.url).hostname):
+            logging.debug(f'Dropping request to a host different than {hostname}: {request.url}.')
             return None
-        if (not hostname in request.url) and (not request.url.endswith(ALLOWED_EXTENSIONS)):
-            # exclude links on other domains (hostnames), unless they are a file with an allowed extensions (files are sometimes hosted on other domains)
+        if sum(self.hit_counter[hostname].values())>MAX_HITS_PER_HOST_TOTAL:
+            logging.debug(f'Max hits per host {hostname} exceeded (reached {sum(self.hit_counter[hostname].values())}), dropping new requests.')
             return None
-        for attr in ('pkl_file', 'index', 'url'):
+        if self.request_counter[hostname]>=MAX_REQUESTS_PER_HOST:
+            logging.debug(f'Max requests ({self.request_counter[hostname]}) reached for {hostname}, dropping new requests.')
+            return None
+        for attr in ('name', 'start_url'):
             request.meta[attr] = response.request.meta.get(attr)
+        self.request_counter[hostname] += 1
         return request
+
 
     def __init__(self, *args, **kwargs):
         super(IsoFinder, self).__init__(*args, **kwargs)
-        #self.pkl_file = kwargs.get('pkl_file')
-        self.host_counter = {}
-        #self.yielded_links = []
+        #self.pkl_file       = kwargs.get('pkl_file')
+        self.request_counter = {}
+        self.hit_counter     = {}
+        self.found_files     = {}
+        self.found_imgs      = {}
 
     def start_requests(self):
+        #df = pd.read_pickle(self.pkl_file)
+        #for url in df.query('url_exists=="TRUE"').url:
         for url in (
                 'https://wettstein-produktion.ch/', # ok
                 'https://juice.world/', #finds way too many links
-                #'http://www.designwerk.com/', #error 510 ??
+                'http://www.designwerk.com/', #error 510
                 'https://www.alz-maschinen.ch/', #ok, needs DEPTH_LIMIT=2
                 'https://aa-praezisionsmechanik.ch/', #no: contains only a png with no keyword in the file name and no reference to keywords in the text
                 'https://www.almutechag.ch/', #ok
                 'https://www.blue-o.ch/', #ok
-                'https://www.hjakober.ch/',
+                'https://www.hjakober.ch/', #ok
+                'https://www.enzler.com', #ok, needs DEPTH_LIMIT=3
+                'https://www.brtec.ch/', #ok
+                'https://www.guyan-trans.ch', #ok, finds pdfs and keywords but no logos
+                'https://www.swissestetic.ch/', #error 510
+                'https://realestate-ch.apleona.com/', #ok, needs DEPTH_LIMIT=4 for pdfs
+                'https://joos-metallbau.ch', #ok
+                'https://www.blumatech.ch/', #ok
+                'https://www.schmidlinag.ch', #ok
+                'https://www.leuthold-metallbau.ch', #ok
+                'http://www.belloli.ch', #ok
+                'https://www.rinoweder.ch/', #ok, false positive images with 'Janisol' in the name
+                'https://www.kazi-metall.ch', #ok but very slow, works with with breadth-first-order search and MAX_REQUESTS_PER_HOST
+                'https://contreag.ch/', #ok
+                'https://www.new-process.ch', #ok
+                'https://www.weita.ch', #ok but very slow
+                'https://www.airproduct.ch', #ok
+                'https://schmid-terewa.ch/', #error 510
                 ):
-            self.host_counter[urlparse(url).hostname] = 0
-            yield Request(url)
-        #files = glob( os.path.join('firms','firms_BS*pkl') )
-        #for pkl_file in files:
-        #    df = pd.read_pickle(pkl_file)
-        #    for index, url in df.query('url_exists=="TRUE"')['url'].iteritems():
-        #        meta = {
-        #                'pkl_file' : pkl_file,
-        #                'index'    : index,
-        #                'url'      : url,
-        #                }
-        #        yield Request(url, self.parse, meta=meta)
+            hostname = urlparse(url).hostname
+            self.request_counter[hostname] = 0
+            self.found_files[hostname]     = []
+            self.found_imgs[hostname]      = []
+            self.hit_counter[hostname]     = {
+                    'page_with_file' : 0,
+                    'page_with_kw'   : 0,
+                    'page_with_logo' : 0,
+                    }
+            meta = {
+                    'name'      : hostname, #FIXME
+                    'start_url' : url,
+                    }
+            yield Request(url, meta=meta)
 
     def parse_item(self, response):
-        hostname = urlparse(response.url).hostname
-        if self.host_counter[hostname]<=MAX_HITS_PER_HOST:
-            if response.url.lower().endswith(ALLOWED_EXTENSIONS) and any(kw in response.url.lower() for kw in KEYWORDS_CERTIFICATE):
-                self.host_counter[hostname] += 1
-                yield {'link_to_file' : response.url}
-            #elif any(kw in response.body.decode('utf-8') for kw in KEYWORDS_URL):
-            elif hasattr(response, 'text') and any([response.xpath(f"//*[contains(translate(text(), 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), '{kw}')]/text()").getall() for kw in KEYWORDS_CERTIFICATE]):
-                self.host_counter[hostname] += 1
-                yield {'link_to_page' : response.url}
-            elif hasattr(response, 'text') and any([link for link in response.css('img').xpath('@src').getall() if any(kw in link.lower() for kw in KEYWORDS_CERTIFICATE)]):
-                self.host_counter[hostname] += 1
-                yield {'link_with_image' : response.url}
-                #for link_to_img in [link for link in response.css('img').xpath('@src').getall() if any(kw in link.lower() for kw in KEYWORDS_CERTIFICATE)]:
-                #    #for embedded images e.g. https://juice.world/
-                #    if not link_to_img.startswith('http'):
-                #        link_to_img = response.urljoin(link_to_img)
-                #    if not link_to_img in self.yielded_links:
-                #        self.yielded_links += [link_to_img]
-                #        yield {'link_to_file' : link_to_img}
+        if hasattr(response, 'text'):
+            hostname = urlparse(response.url).hostname
+            for link_to_file in [link for link in response.css('*::attr(href)').getall() if (urlparse(link).path.lower().endswith(ALLOWED_EXTENSIONS) and any(kw in link.lower() for kw in KEYWORDS_CERTIFICATE))]:
+                if not link_to_file in self.found_files[hostname]:
+                    self.found_files[hostname] += [link_to_file]
+                    #yield {'page_with_file' : (response.url, response.urljoin(link_to_file))}
+                    yield { response.meta['name'] : (response.url, response.urljoin(link_to_file))}
+            if any(text for text in response.css('*::text').getall() if ('ISO' in text or any(kw in text.lower() for kw in KEYWORDS_CERTIFICATE_NO_ISO))):
+                self.hit_counter[hostname]['page_with_kw'] += 1
+                if self.hit_counter[hostname]['page_with_kw']<=MAX_HITS_PER_HOST_PAGE_WITH_KW:
+                    logging.debug(f'found page with keyword at {response.url}')
+                    #yield {'page_with_keyword' : response.url}
+                    yield { response.meta['name'] : (response.url, 'has_keyword')}
+            if any(imgs := [link for link in response.css('img').xpath('@src').getall() if any(kw in link.lower() for kw in KEYWORDS_CERTIFICATE)]):
+                if any(img not in self.found_imgs[hostname] for img in imgs):
+                    self.hit_counter[hostname]['page_with_logo'] += 1
+                    if self.hit_counter[hostname]['page_with_logo']<=MAX_HITS_PER_HOST_PAGE_WITH_IMG:
+                        for found_img in imgs:
+                            if found_img not in self.found_imgs[hostname]:
+                                self.found_imgs[hostname] += [found_img]
+                        logging.debug(f'found page with logo at {response.url}')
+                        #yield {'page_with_logo' : response.url}
+                        yield { response.meta['name'] : (response.url, 'has_logo')}
+
+    def parse_start_url(self, response):
+        return self.parse_item(response)
