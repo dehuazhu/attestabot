c697bd886c14dfdda4477a4dc7cc7fa029fd8f8a
druini <daniele.ruini@cern.ch>
2022-11-16 10:15:02 +0100

    iso-finder mvp, ready to test on full list

--- attestabot/spiders/IsoFinder.py
+++ attestabot/spiders/IsoFinder.py
@@ -35,7 +35,6 @@ class IsoFinder(CrawlSpider):
             'DOWNLOAD_DELAY'         : .25,
             #'ROBOTSTXT_OBEY'         : False,
             'DOWNLOAD_TIMEOUT'       : 15,
-            'LOG_LEVEL'              : 'INFO',
             'DEPTH_LIMIT'            : 4,
             'DEPTH_PRIORITY'         : 1,
             'SCHEDULER_DISK_QUEUE'   : 'scrapy.squeues.PickleFifoDiskQueue',
@@ -56,9 +55,10 @@ class IsoFinder(CrawlSpider):
 
 
     def process_request(self, request, response):
-        hostname = urlparse(response.url).hostname
-        if (hostname != urlparse(request.url).hostname):
-            logging.debug(f'Dropping request to a host different than {hostname}: {request.url}.')
+        hostname = urlparse(response.request.meta.get('company_homepage')).hostname
+        hostname_current = urlparse(response.url).hostname #can be different from hostname because of valid redirects
+        if (hostname_current != urlparse(request.url).hostname):
+            logging.debug(f'Dropping request to a host different than {hostname_current}: {request.url}.')
             return None
         if sum(self.hit_counter[hostname].values())>MAX_HITS_PER_HOST_TOTAL:
             logging.debug(f'Max hits per host {hostname} exceeded (reached {sum(self.hit_counter[hostname].values())}), dropping new requests.')
@@ -66,7 +66,7 @@ class IsoFinder(CrawlSpider):
         if self.request_counter[hostname]>=MAX_REQUESTS_PER_HOST:
             logging.debug(f'Max requests ({self.request_counter[hostname]}) reached for {hostname}, dropping new requests.')
             return None
-        for attr in ('name', 'start_url'):
+        for attr in ('name', 'company_homepage'):
             request.meta[attr] = response.request.meta.get(attr)
         self.request_counter[hostname] += 1
         return request
@@ -79,35 +79,35 @@ class IsoFinder(CrawlSpider):
         self.hit_counter     = {}
 
     def start_requests(self):
-        #df = pd.read_pickle(self.pkl_file)
-        #for url in df.query('url_exists=="TRUE"').url:
-        for url in (
-                'https://wettstein-produktion.ch/', # ok
-                'https://juice.world/', #ok
-                'http://www.designwerk.com/', #error 510
-                'https://www.alz-maschinen.ch/', #ok, needs DEPTH_LIMIT=2
-                'https://aa-praezisionsmechanik.ch/', #no: contains only a png with no keyword in the file name and no reference to keywords in the text
-                'https://www.almutechag.ch/', #ok
-                'https://www.blue-o.ch/', #ok
-                'https://www.hjakober.ch/', #ok
-                'https://www.enzler.com', #ok, needs DEPTH_LIMIT=3
-                'https://www.brtec.ch/', #ok
-                'https://www.guyan-trans.ch', #ok, finds pdfs and keywords but no logos
-                'https://www.swissestetic.ch/', #error 510
-                'https://realestate-ch.apleona.com/', #ok, needs DEPTH_LIMIT=4 for pdfs
-                'https://joos-metallbau.ch', #ok
-                'https://www.blumatech.ch/', #ok
-                'https://www.schmidlinag.ch', #ok
-                'https://www.leuthold-metallbau.ch', #ok
-                'http://www.belloli.ch', #ok
-                'https://www.rinoweder.ch/', #ok, false positive images with 'Janisol' in the name
-                'https://www.kazi-metall.ch', #ok but very slow, works with with breadth-first-order search and MAX_REQUESTS_PER_HOST
-                'https://contreag.ch/', #ok
-                'https://www.new-process.ch', #ok
-                'https://www.weita.ch', #ok but very slow
-                'https://www.airproduct.ch', #ok
-                'https://schmid-terewa.ch/', #error 510
-                ):
+        df = pd.read_pickle(self.pkl_file)
+        for idx, (name,url) in df.query('url_exists=="TRUE"')[['name','url']].iterrows():
+        #for url in (
+        #        'https://wettstein-produktion.ch/', # ok
+        #        'https://juice.world/', #ok
+        #        'http://www.designwerk.com/', #error 510
+        #        'https://www.alz-maschinen.ch/', #ok, needs DEPTH_LIMIT=2
+        #        'https://aa-praezisionsmechanik.ch/', #no: contains only a png with no keyword in the file name and no reference to keywords in the text
+        #        'https://www.almutechag.ch/', #ok
+        #        'https://www.blue-o.ch/', #ok
+        #        'https://www.hjakober.ch/', #ok
+        #        'https://www.enzler.com', #ok, needs DEPTH_LIMIT=3
+        #        'https://www.brtec.ch/', #ok
+        #        'https://www.guyan-trans.ch', #ok, finds pdfs and keywords but no logos
+        #        'https://www.swissestetic.ch/', #error 510
+        #        'https://realestate-ch.apleona.com/', #ok, needs DEPTH_LIMIT=4 for pdfs
+        #        'https://joos-metallbau.ch', #ok
+        #        'https://www.blumatech.ch/', #ok
+        #        'https://www.schmidlinag.ch', #ok
+        #        'https://www.leuthold-metallbau.ch', #ok
+        #        'http://www.belloli.ch', #ok
+        #        'https://www.rinoweder.ch/', #ok, false positive images with 'Janisol' in the name
+        #        'https://www.kazi-metall.ch', #ok but very slow, works with with breadth-first-order search and MAX_REQUESTS_PER_HOST
+        #        'https://contreag.ch/', #ok
+        #        'https://www.new-process.ch', #ok
+        #        'https://www.weita.ch', #ok but very slow
+        #        'https://www.airproduct.ch', #ok
+        #        'https://schmid-terewa.ch/', #error 510
+        #        ):
             hostname = urlparse(url).hostname
             self.request_counter[hostname] = 0
             self.hit_counter[hostname]     = {
@@ -116,38 +116,51 @@ class IsoFinder(CrawlSpider):
                     'page_with_logo' : 0,
                     }
             meta = {
-                    'name'      : hostname, #FIXME
-                    'start_url' : url,
+                    #'name'      : hostname, #FIXME
+                    'name'             : name,
+                    'company_homepage' : url,
                     }
+            logging.info(f'Submitting {url=} with {hostname=}')
             yield Request(url, meta=meta)
 
     def parse_item(self, response):
         if hasattr(response, 'text'):
             item = {
-                    'pkl_file'  : self.pkl_file,
-                    'name'      : response.meta['name'],
-                    'start_url' : response.meta['start_url'],
-                    'found_url' : response.url,
+                    'pkl_file'              : self.pkl_file,
+                    'name'                  : response.meta['name'],
+                    'company_homepage'      : response.meta['company_homepage'],
+                    'suburl_with_iso_info'  : response.url,
+                    'suburl_has_iso_file'   : 'FALSE',
+                    'suburl_is_iso_file'    : 'FALSE',
+                    'suburl_has_keyword'    : 'FALSE',
+                    'suburl_has_logo'       : 'FALSE',
                     }
-            hostname = urlparse(response.url).hostname
+            #hostname = urlparse(response.url).hostname
+            hostname = urlparse(response.meta['company_homepage']).hostname
             if sum(self.hit_counter[hostname].values())<MAX_HITS_PER_HOST_TOTAL:
                 if any(files := [link for link in response.css('*::attr(href)').getall() if (urlparse(link).path.lower().endswith(ALLOWED_EXTENSIONS) and any(kw in link.lower() for kw in KEYWORDS_CERTIFICATE))]):
                     self.hit_counter[hostname]['page_with_file'] += 1
-                    for link_to_file in files:
-                        item['payload'] = response.urljoin(link_to_file)
-                        yield item
+                    item['suburl_has_iso_file'] = 'TRUE'
                 if any(text for text in response.css('*::text').getall() if ('ISO' in text or any(kw in text.lower() for kw in KEYWORDS_CERTIFICATE_NO_ISO))):
                     self.hit_counter[hostname]['page_with_kw'] += 1
                     #if self.hit_counter[hostname]['page_with_kw']<=MAX_HITS_PER_HOST_PAGE_WITH_KW:
                     logging.debug(f'found page with keyword at {response.url}')
-                    item['payload'] = 'has_keyword'
-                    yield item
+                    item['suburl_has_keyword'] = 'TRUE'
                 if any(link for link in response.css('img').xpath('@src').getall() if any(kw in link.lower() for kw in KEYWORDS_CERTIFICATE)):
                     self.hit_counter[hostname]['page_with_logo'] += 1
                     #if self.hit_counter[hostname]['page_with_logo']<=MAX_HITS_PER_HOST_PAGE_WITH_IMG:
                     logging.debug(f'found page with logo at {response.url}')
-                    item['payload'] = 'has_logo'
+                    item['suburl_has_logo'] = 'TRUE'
+                if 'TRUE' in (item['suburl_has_iso_file'], item['suburl_has_keyword'], item['suburl_has_logo']):
                     yield item
+                if files:
+                    item['suburl_is_iso_file']  = 'TRUE'
+                    item['suburl_has_iso_file'] = 'FALSE'
+                    item['suburl_has_keyword']  = 'FALSE'
+                    item['suburl_has_logo']     = 'FALSE'
+                    for link_to_file in files:
+                        item['suburl_with_iso_info'] = response.urljoin(link_to_file)
+                        yield item
 
     def parse_start_url(self, response):
         return self.parse_item(response)
--- iso_finder.py
+++ iso_finder.py
@@ -7,74 +7,72 @@ from scrapy.utils.project import get_project_settings
 from attestabot.spiders.IsoFinder import IsoFinder
 from pdb import set_trace
 
+OUTFILE_RAW   = 'iso_finder_raw.jl'
+INPUT_FOLDER  = 'firms_website_finder'
+OUTPUT_FOLDER = 'firms_iso_finder'
+LOGFILE       = 'iso_finder.log'
 
-def merge_dataframes_and_save(pkl_outfile, to_merge):
-    df_files_kws = pd.merge(to_merge['files'], to_merge['kws'], how='outer').drop_duplicates()
-    df_merged = pd.merge(df_files_kws, to_merge['logos'], how='outer').drop_duplicates()
-    df_merged = pd.concat((df_merged[df_merged.has_file.isna()], df_merged[df_merged.has_file.notna()].drop_duplicates(subset='has_file')))
-    df_merged[df_merged.isna()] = 'FALSE'
-    df_merged.sort_values(by='name', ignore_index=True, inplace=True)
-    #df_merged.to_pickle( pkl_outfile )
-    df_merged.to_excel( pkl_outfile.replace('.pkl','.xlsx') )
-    del to_merge
+def sort_dataframes_and_save(pkl_infile, df_iso_finder):
+    today = datetime.strftime(datetime.now(), '%Y-%m-%d')
+    df_website_finder = pd.read_pickle(pkl_infile).query('url_exists=="TRUE"').rename({'url': 'company_homepage'}, axis='columns')
+    new_columns = []
+    for column in df_website_finder.columns:
+        if column in ('url_exists', 'url_checked_on'):
+            continue
+        if column not in df_iso_finder:
+            new_columns += [column]
+            df_iso_finder[column] = None
+    for idx, (name,company_homepage) in df_iso_finder[['name','company_homepage']].drop_duplicates().iterrows():
+        new_columns_values = df_website_finder.query('name==@name & company_homepage==@company_homepage')
+        if len(new_columns_values)>1: logging.warning(f'Problem with {name}, url: {company_homepage}')
+        df_iso_finder.loc[(df_iso_finder['name']==name) & (df_iso_finder['company_homepage']==company_homepage), new_columns] = new_columns_values.iloc[0][new_columns].values
+
+    df_iso_finder.sort_values(by=['name','company_homepage'], ignore_index=True, inplace=True)
+    df_iso_finder = df_iso_finder[['name', 'ehraid', 'uid', 'uidFormatted', 'chid', 'chidFormatted', 'legalSeatId', 'legalSeat', 'cantonId', 'canton', 'registerOfficeId', 'legalFormId', 'status', 'rabId', 'shabDate', 'deleteDate', 'cantonalExcerptWeb', 'company_homepage', 'suburl_with_iso_info', 'suburl_has_iso_file', 'suburl_is_iso_file', 'suburl_has_keyword', 'suburl_has_logo']].copy()
+    df_iso_finder['iso_checked_on'] = today
+    df_iso_finder['iso_certificate_parsed'] = 'FALSE'
+
+    os.makedirs(OUTPUT_FOLDER, exist_ok=True)
+    outfile = os.path.basename(pkl_infile)
+    outfile = os.path.join(OUTPUT_FOLDER, outfile)
+    #df.to_pickle( pkl_outfile )
+    df_iso_finder.to_excel( outfile.replace('.pkl','.xlsx') )
+    del df_iso_finder, df_website_finder
 
 
 def main():
-    LOGFILE = 'iso_finder.log'
     crawler_settings = get_project_settings()
-    #crawler_settings['LOG_FILE']             = LOGFILE
-    #crawler_settings['LOG_ENABLED']          = False
-    #crawler_settings['LOG_FILE_APPEND']      = False
-    #crawler_settings['LOG_LEVEL']            = 'INFO'
+    crawler_settings['LOG_FILE']             = LOGFILE
+    crawler_settings['LOG_ENABLED']          = True
+    crawler_settings['LOG_FILE_APPEND']      = False
+    crawler_settings['LOG_LEVEL']            = 'INFO'
+    #crawler_settings['LOG_LEVEL']            = 'DEBUG'
     crawler_settings['FEEDS']                = {
-            'iso_finder_raw.jl' : {'format': 'jsonlines'}
+            OUTFILE_RAW : {'format': 'jsonlines'}
             }
-    crawler_process = CrawlerProcess(crawler_settings)
 
-    #files = glob( os.path.join('firms','firms*pkl') )
-    files = ['test.pkl']
+    crawler_process = CrawlerProcess(crawler_settings)
+    files = glob( os.path.join(INPUT_FOLDER, 'firms*pkl') )
+    #files = ['test1115.pkl']
     for pkl_file in files:
         crawler_process.crawl(IsoFinder, pkl_file=pkl_file)
 
-    logging.info('Starting crawl jobs')
+    logging.info(f'Starting crawl jobs')
     crawler_process.start()
 
     logging.info('Done crawling, will now write raw results to Excel files')
 
-    found_files = []
-    found_kws   = []
-    found_logos = []
-
-    with jsonlines.open('iso_finder_raw.jl') as jl_file:
-        for hit in jl_file:
-            #print(hit)
-            if hit['payload']=='has_keyword':
-                found_kws += [pd.Series(hit)]
-            elif hit['payload']=='has_logo':
-                found_logos += [pd.Series(hit)]
-            else:
-                found_files += [pd.Series(hit)]
-    df_files = pd.concat(found_files, axis=1).T.rename({'payload' : 'has_file'}, axis='columns')
-    df_kws   = pd.concat(found_kws, axis=1).T.rename({'payload' : 'has_keyword'}, axis='columns')
-    df_logos = pd.concat(found_logos, axis=1).T.rename({'payload' : 'has_logo'}, axis='columns')
-
-    del found_files, found_kws, found_logos
-
-    df_kws.has_keyword = 'TRUE'
-    df_logos.has_logo  = 'TRUE'
+    with jsonlines.open(OUTFILE_RAW) as jl_file:
+         df = pd.concat((pd.Series(row) for row in jl_file), axis=1).T.drop_duplicates()
 
     df_dict = {
-            pkl_file : {
-                'files' : df_files.query('pkl_file==@pkl_file').drop(columns='pkl_file'),
-                'kws'   : df_kws.query('pkl_file==@pkl_file').drop(columns='pkl_file'),
-                'logos' : df_logos.query('pkl_file==@pkl_file').drop(columns='pkl_file'),
-                }
+            pkl_file : df.query('pkl_file==@pkl_file').drop(columns='pkl_file')
             for pkl_file in files
             }
-    for pkl_outfile, df in df_dict.items():
-        merge_dataframes_and_save(pkl_outfile, df)
+    for pkl_infile, df in df_dict.items():
+        sort_dataframes_and_save(pkl_infile, df)
     #with multiprocessing.Pool() as pool:
-    #    pool.starmap(merge_dataframes_and_save, df_dict.items())
+    #    pool.starmap(sort_dataframes_and_save, df_dict.items())
     logging.info('all done')
 
 
