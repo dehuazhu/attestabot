70a50f400f9e1c1fc805eeebe7be0194fb6cb4f5
druini <daniele.ruini@cern.ch>
2023-01-24 16:26:32 +0100

    adding moneyhouse-scraper

---
+++ attestabot/spiders/MoneyhouseSpider.py
@@ -0,0 +1,95 @@
+import scrapy
+import pandas as pd
+from datetime import datetime
+from pdb import set_trace
+
+class MoneyhouseSpider(scrapy.Spider):
+    name = 'moneyhouse_spider'
+
+    def __init__(self, parquet_file=None, starting_line=0, max_requests_per_file=None, *args, **kwargs):
+        super(MoneyhouseSpider, self).__init__(*args, **kwargs)
+        self.parquet_file          = parquet_file
+        self.starting_line         = starting_line
+        self.max_requests_per_file = max_requests_per_file
+
+    def start_requests(self):
+        url  = 'https://service.moneyhouse.ch/api/login'
+        with open('moneyhouse.login') as f:
+            lines = f.readlines()
+            user  = lines[0].strip()
+            pwd   = lines[1].strip()
+        body = f'{{"email":"{user}","password":"{pwd}"}}'
+
+        yield scrapy.Request(
+                url         = url,
+                method      = 'POST',
+                dont_filter = True,
+                body        = body,
+                callback    = self.make_requests
+                )
+
+
+    def make_requests(self, response):
+        #set_cookie = response.headers[b'Set-Cookie'].decode().split('; ')
+        #user,      = [cookie.replace('user=','') for cookie in set_cookie if cookie.startswith('user=')]
+        #token      = response.json()['token']
+        #cookies    = {
+        #        "token": token,
+        #        "user" : user,
+        #        }
+
+        search_url = lambda query: f'https://www.moneyhouse.ch/de/search?q={query}'
+        df = pd.read_parquet(self.parquet_file)
+
+        start = self.starting_line
+        stop  = start + self.max_requests_per_file
+        for idx, firm in df[start:stop].iterrows():
+            search_str = firm.chid
+            url  = search_url(search_str)
+            meta = {
+                    'parquet_file' : self.parquet_file,
+                    'df_index'     : idx,
+                    }
+
+            yield scrapy.Request(
+                    url         = url,
+                    method      = 'GET',
+                    dont_filter = True,
+                    #cookies     = cookies,
+                    meta        = meta,
+                    callback    = self.parse
+                    )
+
+    def parse(self, response):
+        today    = datetime.strftime(datetime.now(), '%Y-%m-%d')
+        address  = None
+        tel      = None
+        mail     = None
+        homepage = None
+        other    = []
+
+        address_raw, = [cell for cell in response.css('div.section div.card div.section div.l-grid div.l-grid-cell') if cell.css('h4.key::text').get()=='Adresse']
+        if address_raw:
+            address = ', '.join(address_raw.css('p::text').getall()).strip()
+
+        connection_info = response.css('div.connections-row a::attr(href)').getall()
+        for connection in connection_info:
+            if connection.startswith('tel:'):
+                tel = connection.replace('tel:','')
+            elif connection.startswith(('http','www')):
+                homepage = connection
+            elif connection.startswith('mailto:'):
+                mail = connection.replace('mailto:','')
+            else:
+                other += [connection]
+
+        yield {
+                'parquet_file' : response.meta['parquet_file'],
+                'df_index'     : response.meta['df_index'],
+                'checked_on'   : today,
+                'address'      : address,
+                'tel'          : tel,
+                'mail'         : mail,
+                'homepage'     : homepage,
+                'other'        : other,
+                }
---
+++ moneyhouse_scraper.py
@@ -0,0 +1,65 @@
+import os, logging, jsonlines, multiprocessing
+from glob import glob
+import pandas as pd
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+from attestabot.spiders.MoneyhouseSpider import MoneyhouseSpider
+from pdb import set_trace
+
+STARTING_LINE         = 0
+MAX_REQUESTS_PER_FILE = 500
+INPUT_FOLDER  = 'firms_zefix'
+OUTFILE_RAW   = 'moneyhouse_data.jl'
+OUTPUT_FOLDER = 'firms_moneyhouse'
+LOGFILE       = 'moneyhouse_scraper.log'
+
+def write_raw_output_to_files(parquet_file):
+    df = pd.read_parquet(parquet_file)
+    with jsonlines.open(OUTFILE_RAW) as jl_file:
+        rows = (pd.Series(row) for row in jl_file if row['parquet_file']==parquet_file)
+        df_moneyhouse = pd.concat(rows, axis=1).T.drop(columns='parquet_file').set_index('df_index')
+    new_df = df.join(df_moneyhouse)
+
+    os.makedirs(OUTPUT_FOLDER, exist_ok=True)
+    outfile = os.path.basename(parquet_file)
+    outfile = os.path.join(OUTPUT_FOLDER, outfile)
+    new_df.to_parquet( outfile )
+    new_df.to_excel( outfile.replace('.parquet','.xlsx') )
+    del df, df_moneyhouse, new_df
+
+
+def main():
+    files = glob( os.path.join(INPUT_FOLDER, '*.parquet') )
+
+    crawler_settings = get_project_settings()
+    crawler_settings['LOG_ENABLED']    = True
+    #crawler_settings['LOG_FILE']       = LOGFILE
+    crawler_settings['LOG_LEVEL']      = 'INFO'
+    crawler_settings['DOWNLOAD_DELAY'] = 2
+    crawler_settings['FEEDS']          = {
+            OUTFILE_RAW : {
+                'format'    : 'jsonlines',
+                'encoding'  : 'utf8',
+                'overwrite' : False,
+                },
+            }
+
+    crawler_process = CrawlerProcess(crawler_settings)
+    for parquet_file in files:
+        crawler_process.crawl(
+                MoneyhouseSpider,
+                parquet_file          = parquet_file,
+                starting_line         = STARTING_LINE,
+                max_requests_per_file = MAX_REQUESTS_PER_FILE
+                )
+
+    logging.info(f'Starting crawl jobs')
+    #crawler_process.start()
+
+    logging.info(f'Done crawling, will now write raw results to Excel files')
+
+    with multiprocessing.Pool() as pool:
+        pool.map(write_raw_output_to_files, files)
+
+if __name__=='__main__':
+    main()
